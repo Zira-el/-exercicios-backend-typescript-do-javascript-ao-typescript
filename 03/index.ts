d9464b9f23efeae8d18ecfe6d0cb288c4842e968
Nilton Menezes <nilton.menezes@gafit.com.br>
2022-04-13 22:09:11 -0300

    exercicio 3

---
+++ 03/index.ts
@@ -0,0 +1,15 @@
+const numbers: number[] = [0, 122, 4, 6, 7, 8, 44]
+const numbers2: number[] = [0, 122, 4, 6, 8, 44]
+
+const testIfNumbersAreEven = ((arrayNumbers: number[]): string => {
+    const isEven: boolean = arrayNumbers.every(number => number % 2 === 0);
+
+    if (isEven) {
+        return `array válido.`
+    }
+
+    return `array inválido.`
+});
+
+console.log(testIfNumbersAreEven(numbers));
+console.log(testIfNumbersAreEven(numbers2));
